2d07a0fbe9a065692a8c1a58314eb60719848ef7
DipanshiTyagi <143926420+DipanshiTyagi@users.noreply.github.com>
2024-02-12 18:08:37 +0530

    Error handling

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require ("path");
 const axios = require ('axios');
 const app = express();
 
+
 const port = 8080;
 
 
@@ -15,23 +16,34 @@ const uniqueImageParam = () => Math.floor(Math.random() * 1000);
 const fetchImage = async() => {
     const randomImageNumber = uniqueImageParam();
     const imageUrl =  `https://xsgames.co/randomusers/avatar.php?g=male&i=${randomImageNumber}`;
-    const response = await axios.get(imageUrl, {responseType : 'arraybuffer'});
-    return Buffer.from(response.data, 'binary');
+    try {
+        const response = await axios.get(imageUrl, {responseType : 'arraybuffer'});
+        return Buffer.from(response.data, 'binary');
+    } catch(error) {
+        console.error('Error fetching image:', error);
+        throw new Error('Failed to fetch image');
+    }    
 
 }
 
 let getUser = async() => {
-    const [image] = await Promise.all([fetchImage()]);
+    try {
+        const [image] = await Promise.all([fetchImage()]);
 
-    return {
-        image: `data:image/jpeg;base64,${image.toString('base64')}`,
-        name: faker.person.fullName(),
-        rating: faker.number.int({ min: 1, max: 10 }),
-        badge_number: faker.number.int({min: 1, max:999}),
-        phone: faker.phone.number(),
-        noOfTrips: faker.number.int({ min: 1, max: 50 }),
-        location: faker.location.streetAddress()
-    };
+        return {
+            image: `data:image/jpeg;base64,${image.toString('base64')}`,
+            name: faker.person.fullName(),
+            rating: faker.number.int({ min: 1, max: 10 }),
+            badge_number: faker.number.int({min: 1, max:999}),
+            phone: faker.phone.number(),
+            noOfTrips: faker.number.int({ min: 1, max: 50 }),
+            location: faker.location.streetAddress()
+        };
+    } catch(error) {
+        console.error('Error generating user data:', error);
+        throw new Error('Failed to generate user data');
+    }
+    
 };
 
 
@@ -45,11 +57,21 @@ let getUsers = async (count) => {
 };
 
 app.get ('/user', async(req, res) => {
-    const data = await getUsers(25);
-    res.render("data.ejs", {data});
+    try{
+        const data = await getUsers(25);
+        res.render("data.ejs", {data});
+    } catch(error) {
+        next(error);
+    }
 });
 
 console.log(getUser());
+
+//Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something crashed!');
+});
 
 app.listen (port, () => {
     console.log(`Server listening to port ${port}`);
