d1caed7cc8e01e42b12f080bf0b0304a353f0175
DipanshiTyagi <143926420+DipanshiTyagi@users.noreply.github.com>
2024-02-20 02:07:22 +0530

    Delete Backend/db/models directory

--- Backend/db/models/coolie.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const coolieSchema = new mongoose.Schema({
-    data: mongoose.Schema.Types.Mixed
-});
-
-const Coolie = mongoose.model('Coolie', coolieSchema);
-
-module.exports = Coolie;
